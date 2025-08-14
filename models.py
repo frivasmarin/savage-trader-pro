9112e59b520b06feededbc3fdec2151e7d593e3b
frivasmarin <fran_blade_aroxeno@hotmail.com>
2025-08-15 00:34:28 +0200

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from datetime import datetime
-from enum import Enum
-
-class Alert(db.Model):
-    """Model for storing alert history"""
-    id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(10), default='UPST', nullable=False)
-    # [Continúa con todos los modelos...]
